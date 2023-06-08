aac95cdac56dfef098f47726a42eeaf5e11ac78c
lekkimio <Lekkimio15@gmail.com>
2023-06-08 14:42:32 +0300

    initial commit

---
+++ src/main/java/com/example/studentlecture/StudentLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentlecture/StudentLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
